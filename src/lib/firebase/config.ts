ba4cad2dc0bfe39351fb9ecf78ab669c9c363b12
COIN CASH <ycoincash@gmail.com>
2025-09-01 17:01:02 +0000

    Failed to login said email or password is incorrect

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
@@ -6,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDW1ZPSML_OZTmKN35YFANaP4NdQHOPYNs",
-  authDomain: "cashback-56a16.firebaseapp.com",
-  projectId: "cashback-56a16",
-  storageBucket: "cashback-56a16.appspot.com",
-  messagingSenderId: "1090837702640",
-  appId: "1:1090837702640:web:be1142b8b28a374c05df35",
-  measurementId: "G-KKY66CQJ0J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
