5c8214653017a7ecd790e37e41a5a458206f5bf8
COIN CASH <ycoincash@gmail.com>
2025-09-01 02:49:29 +0000

    The problem is related to how you call firebase project

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,15 +3,18 @@ import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/a
 import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDW1ZPSML_OZTmKN35YFANaP4NdQHOPYNs",
+  authDomain: "cashback-56a16.firebaseapp.com",
+  projectId: "cashback-56a16",
+  storageBucket: "cashback-56a16.appspot.com",
+  messagingSenderId: "1090837702640",
+  appId: "1:1090837702640:web:be1142b8b28a374c05df35",
+  measurementId: "G-KKY66CQJ0J"
 };
+
 
 // Initialize Firebase for the client
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
