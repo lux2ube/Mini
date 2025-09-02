92bc2099430e8fea498e43672602cd220cfaeef4
COIN CASH <ycoincash@gmail.com>
2025-09-02 21:02:17 +0000

    FirebaseError: Firebase: Error (auth/invalid-api-key).     at createErro

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "YOUR_API_KEY",
+  authDomain: "cashback-56a16.firebaseapp.com",
+  projectId: "cashback-56a16",
+  storageBucket: "cashback-56a16.appspot.com",
+  messagingSenderId: "395561662919",
+  appId: "1:395561662919:web:968f700b0949d0b6748286",
+  measurementId: "G-L0151593G1"
 };
 
 // This function safely initializes the Firebase app, preventing re-initialization
